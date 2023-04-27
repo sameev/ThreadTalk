db87a9b293249e692bac2a50f79a21643679b97a
Samee Vohra <samee.vohra@gmail.com>
2023-04-27 15:00:07 -0700

    configured tailwind for project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./src/**/*.{html,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
