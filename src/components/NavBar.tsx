3dedbb8c7c55b6b2903571ca5baa9989bd1d2d56
Samee Vohra <samee.vohra@gmail.com>
2023-04-27 16:10:34 -0700

    created navbar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,29 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
   return (
     <>
-      <nav className="nav-bar">
-        <Link className="nav-logo-link" to="/">
-          //TODO: Add a home image
-          <img
-            id="logo"
-            className="nav-logo"
-            src=""
-            alt="logo"
+      <nav className='nav-bar'>
+        <Link className='nav-logo-link' to='/'>
+          <FontAwesomeIcon 
+            id='logo'
+            className='nav-logo'
+            icon={faHouse}
           />
         </Link>
+
+        <ul className="nav-right-list">
+          <li className="nav-message-board-list-item">
+            <Link to='/1' className='nav-message-board-link'>
+              message board
+            </Link>
+          </li>
+        </ul>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
