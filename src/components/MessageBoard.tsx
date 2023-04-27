d5d74775cab21aca82c9ddc9320d45be7adcb6b5
Samee Vohra <samee.vohra@gmail.com>
2023-04-27 16:11:17 -0700

    updated messageboard component with useparams for page#

--- src/components/MessageBoard.tsx
+++ src/components/MessageBoard.tsx
@@ -1,7 +1,14 @@
+import { Link, Outlet } from 'react-router-dom';
+
 const MessageBoard = () => {
   return (
-    <h2>Message Board</h2>
-  )
-}
+    <div className='message-board-container'>
+      <Link to='/1'>
+        <h2 className='message-board-header-link'>Message Board</h2>
+      </Link>
+      <Outlet />
+    </div>
+  );
+};
 
-export default MessageBoard
+export default MessageBoard;
