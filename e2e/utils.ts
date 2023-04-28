239dde846f2bf81c4f6fe4d26f7438b4aab6d242
Samee Vohra <samee.vohra@gmail.com>
2023-04-27 19:43:25 -0700

    added signup util

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,5 +1,6 @@
 import { execSync } from 'child_process';
 import detect from 'detect-port';
+import { Page, expect } from '@playwright/test';
 
 export async function setupE2eTest() {
   await startSupabase();
@@ -18,7 +19,35 @@ async function startSupabase() {
 
 function reseedDb() {
   execSync(
-    "PGPASSWORD=postgres psql -U postgres -h 127.0.0.1 -p 64322 -f supabase/clear-db-data.sql",
-    { stdio: "ignore" }
+    'PGPASSWORD=postgres psql -U postgres -h 127.0.0.1 -p 64322 -f supabase/clear-db-data.sql',
+    { stdio: 'ignore' }
   );
+}
+
+export async function signUp(
+  page: Page,
+  email: string,
+  password: string,
+  userName: string,
+  skipUserName = false
+) {
+  const signUpButton = page.locator("button", { hasText: "Sign Up" }).first();
+  await signUpButton.click();
+  const emailInput = page.locator('input[name="email"]');
+  await emailInput.fill(email);
+  const passwordInput = page.locator('input[name="password"]');
+  await passwordInput.fill(password);
+  await page.keyboard.press("Enter");
+  const welcomeNotice = page.locator("h2", { hasText: "Welcome to Supaship!" });
+  await expect(welcomeNotice).toHaveCount(1);
+  if (skipUserName) {
+    return;
+  }
+  const usernameInput = page.locator('input[name="username"]');
+  await usernameInput.fill(userName);
+  const submitButton = page.locator("button", { hasText: "Submit" });
+  await expect(submitButton).toBeEnabled();
+  await page.keyboard.press("Enter");
+  const logoutButton = page.locator("button", { hasText: "Logout" });
+  await expect(logoutButton).toHaveCount(1);
 }
