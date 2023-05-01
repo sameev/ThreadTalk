6f2b792bf6cd0c696ca30eb3d4abc2e7b88e1736
Samee Vohra <samee.vohra@gmail.com>
2023-05-01 12:56:05 -0700

    added create post and comment utility functions for E2E testing

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,6 +1,7 @@
 import { execSync } from 'child_process';
 import detect from 'detect-port';
 import { Page, expect } from '@playwright/test';
+import { createClient } from '@supabase/supabase-js';
 
 export async function setupE2eTest() {
   await startSupabase();
@@ -19,7 +20,7 @@ async function startSupabase() {
 
 function reseedDb() {
   execSync(
-    'PGPASSWORD=postgres psql -U postgres -h 127.0.0.1 -p 64322 -f supabase/clear-db-data.sql',
+    'PGPASSWORD=postgres psql -U postgres -h 127.0.0.1 -p 54322 -f supabase/clear-db-data.sql',
     { stdio: 'ignore' }
   );
 }
@@ -38,7 +39,7 @@ export async function signUp(
   const passwordInput = page.locator('input[name="password"]');
   await passwordInput.fill(password);
   await page.keyboard.press("Enter");
-  const welcomeNotice = page.locator("h2", { hasText: "Welcome to Supaship!" });
+  const welcomeNotice = page.locator("h2", { hasText: "Welcome to Supaship!!" });
   await expect(welcomeNotice).toHaveCount(1);
   if (skipUserName) {
     return;
@@ -67,10 +68,32 @@ export async function login(
   await emailInput.fill(email);
   const passwordInput = page.locator('input[name="password"]');
   await passwordInput.fill(password);
-  const signUpSubmitButton = page.locator("button.supabase-ui-auth_ui-button");
+  const signUpSubmitButton = page.locator("button.supabase-auth-ui_ui-button");
   await signUpSubmitButton.nth(1).click();
   const logoutButton = page.locator("button", { hasText: "Logout" });
   await expect(logoutButton).toHaveCount(1);
   const usernameMention = page.locator("h2", { hasText: username });
   await expect(usernameMention).toHaveCount(1);
+}
+
+export async function createPost(page: Page, title: string, contents: string) {
+  page.goto("http://localhost:5173/1");
+  const postTitleInput = page.locator(`input[name="title"]`);
+  const postContentsInput = page.locator(`textarea[name="contents"]`);
+  const postSubmitButton = page.locator(`button[type="submit"]`);
+  await postTitleInput.fill(title);
+  await postContentsInput.fill(contents);
+  await postSubmitButton.click();
+  const post = page.locator("h3", { hasText: title });
+  await expect(post).toHaveCount(1);
+  return post;
+}
+
+export async function createComment(page: Page, comment: string) {
+  const commentInput = page.locator(`textarea[name="comment"]`);
+  const commentSubmitButton = page.locator(`button[type="submit"]`);
+  await commentInput.fill(comment);
+  await commentSubmitButton.click();
+  const createdComment = page.locator("p", { hasText: comment });
+  await expect(createdComment).toHaveCount(1);
 }
