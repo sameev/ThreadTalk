0063b2159a58d8f090f5e0ae75f8160ffb572468
Samee Vohra <samee.vohra@gmail.com>
2023-05-01 13:12:29 -0700

    updated playwright config web server

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,8 +70,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:5173',
+    command: 'npm run dev',
+    port: 5173,
     reuseExistingServer: !process.env.CI,
   },
 });
